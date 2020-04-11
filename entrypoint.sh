9b5cd46f1379bafa115c35172cbf6dd965384c01
Sheroy Marker <sheroy@gmail.com>
2020-04-11 14:51:19 -0700

    [Jairo, Sheroy] - Temporarily hard codes the aws region

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
-export AWS_REGION="${INPUT_AWS_REGION}"
+export AWS_REGION="us-west-2"
 WAIT=
 ! "${INPUT_WAIT}" || WAIT='--wait'
 exec /app/ecs-exec --timeout "${INPUT_TIMEOUT}" ${WAIT} --cluster "${INPUT_CLUSTER}" "${INPUT_TASK_NAME}"
