627d952d5028f4ecbf9d78e5f6f26392d7316c00
Sheroy Marker <sheroy@gmail.com>
2020-04-11 15:07:36 -0700

    [Jairo, Sheroy] - Includes the AWS profile in the call to ecs-exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
-export AWS_REGION="us-west-2"
+export AWS_REGION="${INPUT_AWS_REGION}"
 WAIT=
 ! "${INPUT_WAIT}" || WAIT='--wait'
-exec /app/ecs-exec --timeout "${INPUT_TIMEOUT}" ${WAIT} --cluster "${INPUT_CLUSTER}" "${INPUT_TASK_NAME}"
+exec /app/ecs-exec --timeout "${INPUT_TIMEOUT}" ${WAIT} --profile "apfm-dev" --cluster "${INPUT_CLUSTER}" "${INPUT_TASK_NAME}"
