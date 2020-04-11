ecd176a6e032baf551edf17995f4ffca37cd4102
Sheroy Marker <sheroy@gmail.com>
2020-04-11 14:31:30 -0700

    [Jairo, Sheroy] - Fixes the location of the ecs-exec script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
 export AWS_REGION="${INPUT_AWS_REGION}"
 WAIT=
 ! "${INPUT_WAIT}" || WAIT='--wait'
-exec ecs-exec --timeout "${INPUT_TIMEOUT}" ${WAIT} --cluster "${INPUT_CLUSTER}" "${INPUT_TASK_NAME}"
+exec /app/ecs-exec --timeout "${INPUT_TIMEOUT}" ${WAIT} --cluster "${INPUT_CLUSTER}" "${INPUT_TASK_NAME}"
